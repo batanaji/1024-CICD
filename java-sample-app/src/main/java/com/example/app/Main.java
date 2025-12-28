be6c6ec1711adeebb335822484eb5b635a18ffc4
batanaji <batanaji75@gmail.com>
2025-12-28 11:08:54 +0530

    Add files via upload

---
+++ java-sample-app/src/main/java/com/example/app/Main.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java Sample Application!");
+        System.out.println("This is a simple console app for practice.");
+
+        int a = 5;
+        int b = 10;
+        System.out.println("Sum of " + a + " and " + b + " = " + (a + b));
+    }
+}
